ec367dc890f096deee39a88ac8510679efbc81e7
Anders <Andersravn94@gmail.com>
2024-11-25 12:40:48 +0100

    Initial Commit
    Dependecies
    Inital Commit

---
+++ festquestbackend/src/main/java/com/example/festquestbackend/FestquestbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.festquestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestquestbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestquestbackendApplication.class, args);
+	}
+
+}
