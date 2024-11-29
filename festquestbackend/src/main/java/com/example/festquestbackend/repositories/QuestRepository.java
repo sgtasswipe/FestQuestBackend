ba712b8482de22ec78784ba47ac8c6c899269d34
Magne <Magnompower@gmail.com>
2024-11-29 08:37:51 +0100

    Creating the frame of the project

--- festquestbackend/src/main/java/com/example/festquestbackend/repositories/QuestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.festquestbackend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.festquestbackend.models.quests.Quest;
-
-public interface QuestRepository extends JPARepository<Quest, Long> {
-
-}
---
+++ festquestbackend/src/test/java/com/example/festquestbackend/services/QuestServiceTest.java
@@ -0,0 +1,15 @@
+package com.example.festquestbackend.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void findAll() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findById() {
+    }
+
+}
