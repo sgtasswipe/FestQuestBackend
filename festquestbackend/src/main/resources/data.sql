a7dab994a747544249fe859b32f101dc3ca610a3
Anders <Andersravn94@gmail.com>
2024-12-10 09:05:38 +0100

    Remove unused SQL files

--- festquestbackend/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
--- Clear and re-insert roles to ensure they exist
-DELETE FROM roles WHERE name IN ('CREATOR', 'PARTICIPANT');
-INSERT INTO roles (name) VALUES ('CREATOR');
-INSERT INTO roles (name) VALUES ('PARTICIPANT');
