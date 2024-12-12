30d41f3f7e0552d70635bf7604f617c4c23eb3d6
Anders <Andersravn94@gmail.com>
2024-12-12 11:31:18 +0100

    Update Quest.java: improve Quest model to enable description to take more than 10 words. DB Coloum changed from VARCHAR to TEXT

--- festquestbackend/src/main/java/com/example/festquestbackend/models/quests/Quest.java
+++ festquestbackend/src/main/java/com/example/festquestbackend/models/quests/Quest.java
@@ -27,7 +27,7 @@ public class Quest {
     @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition="TEXT")
     private String description;
 
     @Column(nullable = false, length = 1000)
