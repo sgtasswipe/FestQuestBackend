fdd7f210302e1ebf2addb87889fc22c3a3663655
sgtasswipe <otro0001@stud.kea.dk>
2024-12-18 09:47:20 +0100

    updated cors to allow deployment ip

--- festquestbackend/src/main/java/com/example/festquestbackend/config/GlobalCorsConfig.java
+++ festquestbackend/src/main/java/com/example/festquestbackend/config/GlobalCorsConfig.java
@@ -14,7 +14,7 @@ public class GlobalCorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")  // Allow all routes
-                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:63342", "http://40.127.181.161:8080", "http://40.127.181.161")
+                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:63342", "http://40.127.181.161:8080", "http://40.127.181.161:8080")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allow specific HTTP methods
                         .allowedHeaders("Authorization", "Content-Type", "Accept")  // Allow specific headers
                         .exposedHeaders("Authorization")  // Expose headers. Makes it possible to retrieve the Authorization header in the frontend
